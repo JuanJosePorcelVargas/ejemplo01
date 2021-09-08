fce04f091c658b450d97ac2cd9d0dd4821aff885
Juan Jose Porcel Vargas <juanjoseporcel11@gmail.com>
2021-09-08 16:48:43 -0400

    Agregar archivos de proyecto.

---
+++ ejemplo01/Source.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	cout << "nombre:Porcel Vargas Juan Jose\n";
+	cout << "carrera:Ingenieria de Sistemas\n";
+	cout << "CU:35-5079\n";
+	cout << "CI:10369925\n";
+
+		return 0;
+
+}
